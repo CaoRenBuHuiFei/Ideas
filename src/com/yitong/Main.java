07fc307b82326e0fb16e4320d9d4106e5504b6bb
wdf <2119227017@qq.com>
2024-03-01 22:21:24 +0800

    a

---
+++ src/com/yitong/Main.java
@@ -0,0 +1,9 @@
+package com.yitong;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("world");
+        System.out.println("abcdefg");
+    }
+}
